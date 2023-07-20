8442251802bded4b0a40374f1abaf13abc4f64f8
Daniil <daaanyaaooo@yandex.ru>
2023-07-20 19:05:49 +0300

    initial commit

---
+++ android/app/src/main/kotlin/dev/danyaao/viemo/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.danyaao.viemo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/features/movie_details/cubit/movie_details_state.dart
@@ -0,0 +1,10 @@
+part of 'movie_details_cubit.dart';
+
+abstract class MovieDetailsState extends Equatable {
+  const MovieDetailsState();
+}
+
+class MovieDetailsInitial extends MovieDetailsState {
+  @override
+  List<Object> get props => [];
+}
